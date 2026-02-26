02612502529c942db36373b49f4e84f6135b82b7
Jpedrooo <jpedroviegas2005@gmail.com>
2026-02-26 20:24:57 -0300

    Adicionando exercicios de POO

---
+++ POO com Classes, Atributos e Métodos/Relatorio_do_Produto/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto();
+        produto1.nome = "Carro";
+        produto1.preco = 12000.00;
+        produto1.quantidade = 3;
+
+        produto1.exibirInformacoes();
+    }
+}
---
+++ POO com Classes, Atributos e Métodos/Relatorio_do_Produto/Produto.java
@@ -0,0 +1,11 @@
+
+public class Produto {
+    String nome;
+    double preco;
+    int quantidade;
+
+    public void exibirInformacoes(){
+    System.out.printf("Produto: %s - Preco: %.2f - Quantidade: %d%n", nome, preco, quantidade);
+}
+}
+ 
